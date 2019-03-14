3a25c20650b84e40eac4988edf5b367b7332f0d6
sokolova <marysok17@gmail.com>
2019-03-14 10:17:02 +0100

    Removed test dependencies on kx-coroutines-test and android modules from kotlinx-coroutines-core

--- core/kotlinx-coroutines-core/test/internal/ServiceLoaderTest.kt
+++ core/kotlinx-coroutines-core/test/internal/ServiceLoaderTest.kt
@@ -7,14 +7,6 @@ import kotlin.test.assertEquals
 
 class ServiceLoaderTest {
     @Test
-    fun testLoadingSameModuleService() {
-        val providers = MainDispatcherFactory::class.java.let { FastServiceLoader.loadProviders(it, it.classLoader) }
-        assertEquals(providers.size, 2)
-        val providers1 = CoroutineExceptionHandler::class.java.let { FastServiceLoader.loadProviders(it, it.classLoader) }
-        assertEquals(providers1.size, 1)
-    }
-
-    @Test
     fun testCrossModuleService() {
         val providers = CoroutineScope::class.java.let { FastServiceLoader.loadProviders(it, it.classLoader) }
         assertEquals(providers.size, 3)
